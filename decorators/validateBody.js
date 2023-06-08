06f061e0bfbcda5405abc8e12bf1e3c67470ce5b
EugeneKom <EugeneKomissarenko@gmail.com>
2023-06-08 10:27:29 +0200

    added validation for the body

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -3,7 +3,12 @@ const { HttpError } = require("../helpers/index");
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+
+    if (!Object.keys(req.body).length) {
+      throw HttpError(400, "missing fields");
+    }
     if (error) {
+      console.log(error.message);
       next(HttpError(400, `${error.message}`));
     }
     next(error);
